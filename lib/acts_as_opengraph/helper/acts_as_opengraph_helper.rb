dd02d49c54e5e91a0eb84518ac65bf18db16f9d9
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-28 17:17:42 -0700

    Support passing in parameters when calling opengraph_meta_tags_for

--- lib/acts_as_opengraph/helper/acts_as_opengraph_helper.rb
+++ lib/acts_as_opengraph/helper/acts_as_opengraph_helper.rb
@@ -6,12 +6,23 @@ module ActsAsOpengraphHelper
   # @raise [ArgumentError] When you pass an instance of an object that doesn't responds to opengraph_data (maybe you forgot to add acts_as_opengraph in your model)
   # @example
   #   opengraph_meta_tags_for(@movie)
-  def opengraph_meta_tags_for(obj)
+  def opengraph_meta_tags_for(obj, options = {})
     raise(ArgumentError.new, "You need to call acts_as_opengraph on your #{obj.class} model") unless obj.respond_to?(:opengraph_data)
+
+
     tags = obj.opengraph_data.map do |att|
+			if options.is_a?(Hash)
+				key = att[:name].include?("og:") ? att[:name].split("og:")[1] : att[:name]
+				if options.include?(key.to_sym)
+					att[:value] = options.delete(key.to_sym) 
+				end
+			end
       att_name = att[:name] == "og:site_name" ? att[:name] : att[:name].dasherize
       %(<meta property="#{att_name}" content="#{Rack::Utils.escape_html(att[:value])}"/>)
     end
+		options.each do |key, value|
+			tags.push( %(<meta property="og:#{key}" content="#{Rack::Utils.escape_html(value)}"/>))
+		end
     tags = tags.join("\n")
     tags.respond_to?(:html_safe) ? tags.html_safe : tags
   end
